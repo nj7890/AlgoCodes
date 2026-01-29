5a3e03c8de80b9072356b3977bda055c46921b1f
imakshat47 <imakshat47@gmail.com>
2026-01-29 22:47:11 +0530

    Update from Lappy by ImAkshat47 - go/ at 2026-01-29_22-47-02_+05:30

---
+++ go/helloWorld.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	str := "Hello world !"
+	var str1 string = "Hello World !!"
+	var a bool
+	fmt.Println(str)
+	fmt.Println(str1)
+	fmt.Print(a)
+}
